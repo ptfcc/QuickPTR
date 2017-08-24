aa80ed155d3f10851baf006aaf7ac0b9eb1a68b3
Francesco Cifariello Ciardi <f.cifariellociardi@gmail.com>
2017-08-24 20:27:50 +0100

    Use GLM for math and first definitions.

---
+++ QuickPTR/Main.cpp
@@ -0,0 +1,10 @@
+// This is just a very tmp for now. 
+#include "Math/MathUtils.h"
+#include <iostream>
+
+int main()
+{
+	Float2 aVector = Float2(1.0f, 1.0f);
+	Float2 anotherVec = Float2(2.0f, 3.53f);
+	return 0;
+}
---
+++ QuickPTR/Math/MathUtils.h
@@ -0,0 +1,13 @@
+#include "thirdparty/glm/vec2.hpp"
+#include "thirdparty/glm/vec3.hpp"
+#include "thirdparty/glm/vec4.hpp"
+#include "thirdparty/glm/mat4x4.hpp"
+#include "thirdparty/glm/matrix.hpp"
+
+using Float2 = glm::vec2;
+using Float3 = glm::vec3;
+using Float4 = glm::vec4;
+using Int2 = glm::ivec2;
+using Int3 = glm::ivec3;
+using Int4 = glm::ivec4;
+using Mat4 = glm::mat4x4;
